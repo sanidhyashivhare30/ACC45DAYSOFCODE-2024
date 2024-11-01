cf75d2246e334fd0c09594b938eac7b57a6000b3
Sanidhya Shivhare <sanidhyashivhare30@gmail.com>
2024-11-01 11:58:58 +0530

    Add files via upload

---
+++ Day 26- Mutated Minions.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    printf("The number of test cases: ");
+    scanf("%d", &T);
+
+    while(T--) {
+        int N, K, count = 0;
+        scanf("%d %d", &N, &K);
+
+        for(int i = 0; i < N; i++) {
+            int characteristic;
+            scanf("%d", &characteristic);
+
+            if((characteristic + K) % 7 == 0) {
+                count++;
+            }
+        }
+        printf("%d\n", count);
+    }
+
+    return 0;
+}        
