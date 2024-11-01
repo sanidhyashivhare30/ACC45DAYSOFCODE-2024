1c28eeccf243b1dcd07d70a2f0d2b8090b4e61e2
Sanidhya Shivhare <sanidhyashivhare30@gmail.com>
2024-11-01 12:32:58 +0530

    Add files via upload

---
+++ Day 28- Reach fast.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int T;
+    printf("The number of test cases: ");
+    scanf("%d", &T);
+
+    while(T--) {
+        int A, B, K;
+        scanf("%d %d %d", &A, &B, &K);
+
+        int distance = abs(B - A);
+        int steps = distance / K;
+        if(distance % K != 0) {
+            steps++;
+        } 
+        printf("%d\n", steps);
+    }
+
+    return 0;
+}
