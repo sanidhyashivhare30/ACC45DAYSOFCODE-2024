2b60485f9ad5fd283ac1d443567a2089cce1961a
Sanidhya Shivhare <sanidhyashivhare30@gmail.com>
2024-11-01 11:30:02 +0530

    Add files via upload

---
+++ Day 25- Maximise the Tastiness.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    printf("The number of test cases: ");
+    scanf("%d", &T);
+
+    while(T--) {
+        int a, b, c, d;
+        scanf("%d %d %d %d", &a, &b, &c, &d);
+
+        int max_tastiness = (a > b ? a : b) + (c > d ? c : d);
+        printf("%d\n", max_tastiness);
+    }
+
+    return 0;
+}
