dad1c4b2d900bc18f15c71e0e83fac7a11429d0c
Sanidhya Shivhare <sanidhyashivhare30@gmail.com>
2024-11-04 11:57:39 +0530

    Add files via upload

---
+++ Day 30- Is the Score Consistent.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    printf("The number of test cases: ");
+    scanf("%d", &T);
+
+    while(T--) {
+        int A, B, C, D;
+        scanf("%d %d", &A, &B);
+        scanf("%d %d", &C, &D);
+
+        if(C >= A && D >= B) {
+            printf("POSSIBLE\n");
+        } else {
+            printf("IMPOSSIBLE\n");
+        }
+    }
+
+    return 0;
+}
